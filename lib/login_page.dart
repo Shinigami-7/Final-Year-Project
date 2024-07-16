80ee15904d608d70237ade23d5034e7581a60c87
Lhakpa Tsheri Sherpa <lhakpasherpa090@gmail.com>
2024-07-16 16:22:15 +0545

    adding home page and change in signup page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'home.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -10,9 +10,24 @@ class LoginPage extends StatelessWidget {
         title: const Text('Login Page'),
       ),
       body: Center(
-        child: Text(
-          'This is the Login Page',
-          style: TextStyle(fontSize: 24),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Home()),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+          ),
+          child: Text(
+            'LOGIN',
+            style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
