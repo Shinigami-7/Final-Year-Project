66f36ee5baacfbf7533fcaf7d138e38a7da85957
Shinigami <REEZANSHRESTHA15@GMAIL.COM>
2024-07-17 13:43:01 +0545

    adding create new account text and navagating it to signup page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:projectk/home.dart';
+import 'package:projectk/signup_page.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -54,72 +56,6 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
 
-<<<<<<< HEAD
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Container(
-                    width: 170,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const LoginPage()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                      ),
-                      child: Text(
-                        'LOG IN',
-                        style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Text("or", style: TextStyle(fontSize: 20),),
-          Container(
-            width: 240,
-            height: 50,
-            child: ElevatedButton.icon(
-              onPressed: (){},
-              icon: Icon(Icons.facebook), // ReflixIcon to display on the left of the button text
-              label: Text('Continue with Facebook'), // Text to display on the button
-              style: ElevatedButton.styleFrom(
-                // Style customization for the button
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-
-            ),
-          ),
-          SizedBox(height: 10,),
-          Container(
-            width: 240,
-            height: 50,
-            child: ElevatedButton(
-              onPressed: (){},
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              child: Row(
-                children: [
-                  Image.asset('assets/images/google.png', height: 20,width: 50,),
-                  Text('Continue with Google'),
-                ],
-              ),
-
-
-            ),
-          ),
-=======
               Padding(
                 padding: const EdgeInsets.only(top: 10.0),
                 child: Container(
@@ -146,20 +82,69 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 10,),
-
               Text("or", style: TextStyle(fontSize: 20),),
-              ElevatedButton(
-                onPressed: () {
-                  print('done');
-                },
-                style: ButtonStyle(shape: WidgetStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0), // Adjust the radius as needed
-                ),)),
+              Container(
+                width: 240,
+                height: 40,
+                child: ElevatedButton.icon(
+                  onPressed: (){},
+                  icon: Icon(Icons.facebook), // ReflixIcon to display on the left of the button text
+                  label: Text('Continue with Facebook'), // Text to display on the button
+                  style: ElevatedButton.styleFrom(
+                    // Style customization for the button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
 
-                child: Text('Continue with facebook'),
+                ),
               ),
+              SizedBox(height: 10,),
+              Container(
+                width: 240,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/google.png', height: 20,width: 50,),
+                      Text('Continue with Google'),
+                    ],
+                  ),
 
->>>>>>> c29e09df3dfb9c6ec263ebcef6e5d45527c0c67a
+
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 2,
+                ),
+              ),
+                 Padding(
+                   padding: const EdgeInsets.all(10.0),
+                   child: GestureDetector(
+                     onTap: (){
+                   Navigator.push(
+                   context,
+                   MaterialPageRoute(builder: (context) => SignUpScreen()),
+                   );
+                     },
+                     child:Text("Create new account",style: TextStyle(color: Colors.white),),
+                   ),
+                 ),
+
+
+
+
+
+
             ],
           ),
         ),
