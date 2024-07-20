6a435bcb5c444529c43d71c5e87a4342b34c1072
Shinigami <REEZANSHRESTHA15@GMAIL.COM>
2024-07-20 22:29:11 +0545

    adding profile page

---
+++ lib/profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: const Center(
+        child: Text('This is the profile page'),
+      ),
+    );
+  }
+}
