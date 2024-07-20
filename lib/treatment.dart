1703490b932445fbb4f5dcfb2cc30bbe98135d2d
Shinigami <REEZANSHRESTHA15@GMAIL.COM>
2024-07-20 23:44:06 +0545

    adding component in appointment srcreen

--- lib/treatment.dart
+++ lib/treatment.dart
@@ -1,62 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:projectk/appointment.dart';
-import 'package:projectk/profile.dart';
-void main() {
-  runApp(const MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+import 'package:projectk/home.dart';
+import 'package:projectk/report.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bottom Navigation Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
-        ),
-      ),
-      home: const Appointment(),
+      home: Treatment(),
     );
   }
 }
 
-class Treatment extends StatelessWidget {
-  const Treatment({Key? key}) : super(key: key);
+class Treatment extends StatefulWidget {
+  @override
+  _TreatmentState createState() => _TreatmentState();
+}
+
+class _TreatmentState extends State<Treatment> {
+  int _currentIndex = 3;
+  final List<Widget> _screens = [
+    Home(),
+    Appointment(),
+    Report(),
+    Treatment(), // Include the Treatment screen in the list
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => _screens[index]),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('My App'),
         actions: [
           IconButton(
             icon: Icon(Icons.notifications),
             onPressed: () {
-              GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Profile()),
-                );
-              }
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Treatment()),
               );
-              // Handle notification button press
             },
           ),
         ],
-
       ),
-      body: const Center(
-        child: Text('This is the Treatment page'),
-      ),
+      body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.blue,
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.grey,
-        currentIndex: 3,
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.today),
