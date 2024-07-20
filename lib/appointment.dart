2b6d39871620578f61d17e4c4d62828b985d6ba1
Shinigami <REEZANSHRESTHA15@GMAIL.COM>
2024-07-20 23:09:44 +0545

    making some adjustment in all page

--- lib/appointment.dart
+++ lib/appointment.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:projectk/profile.dart';
+import 'package:projectk/treatment.dart';
+
 class Appointment extends StatelessWidget {
   const Appointment({Key? key}) : super(key: key);
 
@@ -7,14 +10,49 @@ class Appointment extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: const Icon(Icons.notifications, color: Colors.blue),
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Treatment()),
+                    );
+                  }
+              );
+              // Handle notification button press
+            },
           ),
         ],
       ),
       body: const Center(
         child: Text('This is the Appointment page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        currentIndex: 1,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.today),
+            label: 'Today',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Appointment',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.report),
+            label: 'Report',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.medical_services),
+            label: 'Treatment',
+          ),
+        ],
       ),
     );
   }
