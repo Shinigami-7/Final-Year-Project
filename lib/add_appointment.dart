6495e5a96be9b95413866e195ce2b1d18ecda034
Shinigami <REEZANSHRESTHA15@GMAIL.COM>
2024-08-20 22:26:13 +0545

    adding new add_appointment screen

---
+++ lib/add_appointment.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'notification.dart';
+
+class AddAppointment extends StatelessWidget {
+  const AddAppointment({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: IconButton(
+              icon: Icon(Icons.notification_add, color: Colors.white, size: 30),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NotificationPage()),
+                );
+              },
+            ),
+          ),
+        ],
+        title: Text("Appointment", style: TextStyle(color: Colors.white),
+        ),
+      ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 60, left: 20, right: 20, bottom: 20),
+              child: Column(
+                children: [
+                  Text('What is the purpose', style: TextStyle(fontSize: 18, color: Colors.white)),
+                  Container(
+                    width: 200,
+                    child: TextField(
+                      autofocus: false,
+
+                      decoration: InputDecoration(
+
+                        hintText: 'for checkup',
+
+                      ),
+                    ),
+                  )
+                ],
+              ),
+
+          ),
+        ],
+      ),
+    );
+  }
+}
